746ebea07d69077f1cfd488ead6ead00d519e9b5
Peiyun <b612starnight@gmail.com>
2023-01-04 17:08:31 +0800

    Initial Commit

---
+++ PhotoInUIAlertController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoInUIAlertController
+//
+//  Created by Peiyun on 2023/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
